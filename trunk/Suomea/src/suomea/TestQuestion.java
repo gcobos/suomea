e449dd49ddf2de3e31fff5413a4f98e430fd9011
gcobos <gcobos@7d5d580d-d173-f6ea-fa72-c4d07abcb1d9>
2010-10-10 12:38:12 +0000

    La clase TestExercise con el método getExercise genera y devuelve un ejercicio listo para usar

---
+++ trunk/Suomea/src/suomea/TestQuestion.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package suomea;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author drone
+ */
+public class TestQuestion {
+
+    public String word;
+    
+    public ArrayList options;
+
+}
