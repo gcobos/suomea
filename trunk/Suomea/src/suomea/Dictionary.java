95ad4ad284bebeb26e7ae114201d8b2f32a97372
gcobos <gcobos@7d5d580d-d173-f6ea-fa72-c4d07abcb1d9>
2010-10-10 16:44:07 +0000

    generates exercise

--- trunk/Suomea/src/suomea/Dictionary.java
+++ trunk/Suomea/src/suomea/Dictionary.java
@@ -6,6 +6,7 @@
 package suomea;
 
 import java.sql.ResultSet;
+import java.util.Random;
 
 
 
@@ -21,9 +22,17 @@ public class Dictionary {
 
         ResultSet rs;
         String[] result = new String[2];
+        int num = 0;
+        Random rand;
 
         try {
-            rs = db.query("SELECT original, translation FROM words WHERE rowid=34;");
+            rs = db.query("SELECT COUNT(*) FROM words;");
+            if (rs.next()) {
+                num = rs.getInt(1);
+            }
+            rand = new Random();
+
+            rs = db.query("SELECT original, translation FROM words WHERE rowid="+ rand.nextInt(num) + ";");
             if (rs.next()) {
                  result[0] = rs.getString("original");
                  result[1] = rs.getString("translation");
