87902d61c79ad853c3fd769f5851de87c174acf8
nolssi <nolssi@7d5d580d-d173-f6ea-fa72-c4d07abcb1d9>
2010-10-18 19:11:35 +0000

    Header of the interfaces

--- trunk/Suomea/src/suomea/modules/exercises/IExercise.java
+++ trunk/Suomea/src/suomea/modules/exercises/IExercise.java
@@ -1,8 +1,20 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright 2007-2010
+ * This file is part of Suomea.
+ *
+ * Suomea is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * Suomea is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Suomea; if not, write to the Free Software Foundation, Inc., 51 Franklin St,
+ * Fifth Floor, Boston, MA 02110-1301 USA
  */
-
 package suomea.modules.exercises;
 
 /**
@@ -11,17 +23,14 @@ package suomea.modules.exercises;
  */
 public interface IExercise {
 
-    public void prepare ();
+    public void prepare();
 
-    public IQuestion getQuestion (int id);
+    public IQuestion getQuestion(int id);
 
     public int getNumberOfQuestions();
 
     public double getScore();
 
-
-
-    // Closes the exersize and update statistics
+    // Closes the exercise and update statistics
     public void finish();
-
 }
